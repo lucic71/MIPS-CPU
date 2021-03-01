c2e32516ed920925e15586925d9a28b7f3c9a401
lucic71 <lucian.popescu187@gmail.com>
2021-03-01 10:25:54 +0200

    Changed opcodes localparam to define

--- CPU.srcs/sources_1/new/ALU.v
+++ CPU.srcs/sources_1/new/ALU.v
@@ -49,39 +49,39 @@ module ALU(
             case (alu_op[4:1])
                 // TODO: implement signed and unsigned addition and
                 // substraction
-                OPCODE_ADD: begin
+                `OPCODE_ADD: begin
                     result_int = reg_1 + reg_2;
                     take_branch_int = 1'b0;
                 end
-                OPCODE_SUB: begin
+                `OPCODE_SUB: begin
                     result_int = reg_1 - reg_2;
                     take_branch_int = 1'b0;
                 end
-                OPCODE_OR: begin
-                    result_int = reg_1 | reg_2;
+                `OPCODE_OR: begin
+                    result_int[15:0] = reg_1 | reg_2;
                     take_branch_int = 1'b0;
                 end
-                OPCODE_XOR: begin
-                    result_int = reg_1 ^ reg_2;
+                `OPCODE_XOR: begin
+                    result_int[15:0] = reg_1 ^ reg_2;
                     take_branch_int = 1'b0;
                 end
-                OPCODE_AND: begin
-                    result_int = reg_1 & reg_2;
+                `OPCODE_AND: begin
+                    result_int[15:0] = reg_1 & reg_2;
                     take_branch_int = 1'b0;
                 end
-                OPCODE_NOT: begin
+                `OPCODE_NOT: begin
                     result_int = ~reg_1;
                     take_branch_int = 1'b0;
                 end
-                OPCODE_READ: begin
-                    result_int = reg_1 + imm[4:0];
+                `OPCODE_READ: begin
+                    result_int[15:0] = reg_1 + imm[4:0];
                     take_branch_int = 1'b0;
                 end
-                OPCODE_WRITE: begin
-                    result_int = reg_1 + imm[15:11];
+                `OPCODE_WRITE: begin
+                    result_int[15:0] = reg_1 + imm[15:11];
                     take_branch_int = 1'b0;
                 end
-                OPCODE_LOAD: begin
+                `OPCODE_LOAD: begin
                     if (alu_op[0:0])
                         result_int[15:0] = {imm[7:0], 8'b0};
                     else begin
@@ -89,7 +89,7 @@ module ALU(
                         take_branch_int = 1'b0;
                     end
                 end
-                OPCODE_CMP: begin
+                `OPCODE_CMP: begin
                     result_int[CMP_BIT_EQ:CMP_BIT_EQ] = (reg_1 == reg_2);
                     result_int[CMP_BIT_AZ:CMP_BIT_AZ] = (reg_1 == 16'h0000);
                     result_int[CMP_BIT_BZ:CMP_BIT_BZ] = (reg_2 == 16'h0000);
@@ -105,20 +105,20 @@ module ALU(
                     result_int[9:0] = 10'b0;
                     take_branch_int = 1'b0;
                 end
-                OPCODE_SHL: begin
+                `OPCODE_SHL: begin
                     result_int[15:0] = left_shift_result;
                     take_branch_int = 1'b0;
                 end
-                OPCODE_SHR: begin
+                `OPCODE_SHR: begin
                     result_int[15:0] = right_shift_result;
                     take_branch_int = 1'b0;
                 end
-                OPCODE_JUMP: begin
+                `OPCODE_JUMP: begin
                     if (alu_op[0:0] == 1'b0)
                         result_int[15:0] = reg_1;
                     take_branch_int = 1'b1;
                 end
-                OPCODE_JUMPEQ: begin
+                `OPCODE_JUMPEQ: begin
                     result_int[15:0] = reg_2;
 
                     case ({alu_op[0:0], imm[1:0]})
--- CPU.srcs/sources_1/new/OPCODES.vh
+++ CPU.srcs/sources_1/new/OPCODES.vh
@@ -1,19 +1,19 @@
 `ifndef _opcodes_h
 `define _opcodes_h
 
-localparam OPCODE_ADD    = 4'b0000;
-localparam OPCODE_SUB    = 4'b0001;
-localparam OPCODE_OR     = 4'b0010;
-localparam OPCODE_XOR    = 4'b0011;
-localparam OPCODE_AND    = 4'b0100;
-localparam OPCODE_NOT    = 4'b0101;
-localparam OPCODE_READ   = 4'b0110;
-localparam OPCODE_WRITE  = 4'b0111;
-localparam OPCODE_LOAD   = 4'b1000;
-localparam OPCODE_CMP    = 4'b1001;
-localparam OPCODE_SHL    = 4'b1010;
-localparam OPCODE_SHR    = 4'b1011;
-localparam OPCODE_JUMP   = 4'b1100;
-localparam OPCODE_JUMPEQ = 4'b1101;
+`define OPCODE_ADD    4'b0000
+`define OPCODE_SUB    4'b0001
+`define OPCODE_OR     4'b0010
+`define OPCODE_XOR    4'b0011
+`define OPCODE_AND    4'b0100
+`define OPCODE_NOT    4'b0101
+`define OPCODE_READ   4'b0110
+`define OPCODE_WRITE  4'b0111
+`define OPCODE_LOAD   4'b1000
+`define OPCODE_CMP    4'b1001
+`define OPCODE_SHL    4'b1010
+`define OPCODE_SHR    4'b1011
+`define OPCODE_JUMP   4'b1100
+`define OPCODE_JUMPEQ 4'b1101
 
 `endif
